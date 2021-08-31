ce16e7ee34326c6097c2c8f570a493d53623113e
nguyentamthanh <77631594+nguyentamthanh@users.noreply.github.com>
2021-08-31 10:09:17 +0700

    tamtahnh

---
+++ Controllers/DemoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GiaiptBac1.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Timx()
+        {
+            return View();
+        }
+        public ActionResult Timx(string soA, string soB)
+        {
+            double so1 = Convert.ToDouble;
+            double so2 = Convert.ToDouble;
+            double gtx = so2 / so1;
+            ViewBag.timgtx = gtx;
+            return View();
+        }
+    }
+}
---
+++ Models/GPT.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GiaiptBac1.Models
+{
+    public class GPT
+    {
+        public double Timx(double a , double b)
+        {
+            double gtx = 0;
+            gtx=b/a;
+            return gtx;
+        }
+    }
+}
