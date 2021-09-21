41f5348601c47a7b33a38f7938794914b256b926
nguyentamthanh <77631594+nguyentamthanh@users.noreply.github.com>
2021-09-21 16:16:19 +0700

    4:16

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GiaiptBac1.Models;
+
+namespace GiaiptBac1.Controllers
+{
+    public class StudentController : Controller
+    {
+        LTQLDbContext db = new LTQLDbContext();
+
+        // GET: Student
+        public ActionResult Index()
+        {
+            var model = db.Students.ToList();
+            return View(model);
+        }
+       
+    }
+}
