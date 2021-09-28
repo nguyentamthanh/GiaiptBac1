fb40e5c57c8a6b23a00299813be5e720ecba605a
nguyentamthanh <77631594+nguyentamthanh@users.noreply.github.com>
2021-09-28 09:25:06 +0700

    28/9

--- Models/Student.cs
+++ Models/Student.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
+using GiaiptBac1.Models.Process;
 
 namespace GiaiptBac1.Models
 {
